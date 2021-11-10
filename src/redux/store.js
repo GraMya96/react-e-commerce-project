3edc2585d547ec62857c2a6bedfca61813646c62
Matteo Gramegnatota <m.gramegnatota@77agency.com>
2021-11-10 17:21:41 +0100

    Removed Redux Logger from Production

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,14 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [ logger ];
 
+if( process.env.NODE_ENV === 'development' ) {
+    // A variable stored in Node which reate-react-app
+    // makes accessible through process.env and
+    // which changes depending on whether we are in
+    // 'npm start' mode or 'npm run build' mode
+    middlewares.push( logger );
+}
+
 export const store = createStore( rootReducer, applyMiddleware( ...middlewares ) );
 
 export const persistor = persistStore( store );
