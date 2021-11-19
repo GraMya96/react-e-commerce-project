9b017cfa724caac6a0ebf26ad00e0d5e3e36e35d
Matteo Gramegnatota <m.gramegnatota@77agency.com>
2021-11-19 18:09:55 +0100

    Added Error Message to Add to Cart when user not logged in

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -58,7 +58,7 @@ const CheckoutPage = () => {
             <div className="test-warning">
                 *Please use the following test credit card for payment:
                 <br />
-                4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+                4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
             </div>
             <StripeButton
                 price = { totalPrice } />
